2b54c49ac7bc3a835f8c39e417be9cfaaf7f432b
Janssen Batista <batistajanssen.dev@gmail.com>
2024-05-03 00:43:24 -0300

    buid: add voyager configuration

--- app/src/main/java/dev/janssenbatista/shoppinglist/MainActivity.kt
+++ app/src/main/java/dev/janssenbatista/shoppinglist/MainActivity.kt
@@ -3,6 +3,9 @@ package dev.janssenbatista.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
+import dev.janssenbatista.shoppinglist.ui.screens.splash.SplashScreen
 import dev.janssenbatista.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +13,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingListTheme {
-
+                Navigator(SplashScreen) {
+                    SlideTransition(navigator = it)
+                }
             }
         }
     }
